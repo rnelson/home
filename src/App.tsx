709fbedf211bce73ac3b8b3a552449bbbb62f0e6
Ross Nelson <ross.nelson@gmail.com>
2025-04-30 12:05:40 -0500

    Start a search feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Weather from "./components/Weather.tsx";
+import WebSearch from "./components/WebSearch.tsx";
 
 const App = () => {
 
 	return (
 		<div>
+			<WebSearch/>
 			<Weather/>
 		</div>
 	);
---
+++ src/components/WebSearch.tsx
@@ -0,0 +1,16 @@
+﻿const DoWebSearch = (data: FormData) => {
+	alert(`Search: ${data.get("query")}`);
+};
+
+const WebSearch = () => {
+	return (
+		<div className="p-16">
+			<form action={DoWebSearch}>
+				<input className="bg-neutral-50 text-neutral-950 p-2 rounded-lg w-[900px]" name="query"/>
+				<button type="submit">Search</button>
+			</form>
+		</div>
+	);
+};
+
+export default WebSearch;
